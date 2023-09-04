b3cce330095c46d18d426a2398406305a82536e7
shoebmalik07 <malik.shoeb@alamanceinc.com>
2023-09-04 17:10:50 +0530

    bugfix/modified navigations

--- src/Screens/MapScreen.tsx
+++ src/Screens/MapScreen.tsx
@@ -144,7 +144,7 @@ const MapScreen = ({ route }: any) => {
   // };
 
   const clickHandler = () => {
-    navigation.navigate("CalendarScreen", {
+    navigation.navigate("FindFoodHomeScreen", {
       location: location,
       city: city,
       state: state,
